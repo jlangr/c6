ed445b46b3ec98ce9ebbec98eabdb9d49f13a672
jlangr <jeff@langrsoft.com>
2013-01-02 10:39:48 -0700

    const

--- PortfolioTest.cpp
+++ PortfolioTest.cpp
@@ -2,29 +2,42 @@
 #include "Portfolio.h"
 
 using namespace ::testing;
+using namespace std;
 
+// START:const
 class APortfolio: public Test {
 public:
+// START_HIGHLIGHT
+   static const string IBM;
+// END_HIGHLIGHT
    Portfolio portfolio_;
 };
-
+// START_HIGHLIGHT
+const string APortfolio::IBM("IBM");
+// END_HIGHLIGHT
+// ...
+// END:const
 TEST_F(APortfolio, IsEmptyWhenCreated) {
    ASSERT_THAT(portfolio_.IsEmpty(), Eq(true));
 }
 
+// START:const
 TEST_F(APortfolio, IsNotEmptyAfterPurchase) {
-   portfolio_.Purchase("IBM", 1);
+   portfolio_.Purchase(IBM, 1);
 
    ASSERT_THAT(portfolio_.IsEmpty(), Eq(false));
 }
-
+// ...
+// END:const
 TEST_F(APortfolio, AnswersZeroForSharesOfUnpurchasedSymbol) {
    ASSERT_THAT(portfolio_.Shares("AAPL"), Eq(0));
 }
 
+// START:const
 TEST_F(APortfolio, AnswersSharesForPurchasedSymbol) {
-   portfolio_.Purchase("IBM", 2);
+   portfolio_.Purchase(IBM, 2);
 
-   ASSERT_THAT(portfolio_.Shares("IBM"), Eq(2));
+   ASSERT_THAT(portfolio_.Shares(IBM), Eq(2));
 }
+// END:const
 
